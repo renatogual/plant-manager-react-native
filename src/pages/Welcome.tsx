291d602b6f02c2f508c47ca7a7cef85e9661c4f4
renatogual <renatogual@hotmail.com>
2021-04-20 11:21:00 -0300

    Update componente Welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,23 +1,53 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, StatusBar, Image } from 'react-native';
 
-import wateringImg from "../assets/watering.png";
+import wateringImg from '../assets/watering.png';
+import Button from '../components/Button';
+import colors from '../styles/colors';
 
 export function Welcome() {
   return (
-    <View style={styles.container}>
-      <Text>Gerencie suas plantas de forma fácil</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}
+        suas plantas {'\n'}
+        de forma fácil
+      </Text>
 
-      <Image source={wateringImg} />
-    </View>
+      <Image style={styles.image} source={wateringImg} />
+
+      <Text style={styles.subTitle}>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
+
+      <Button title=">" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: StatusBar.currentHeight ? StatusBar.currentHeight + 38 : 38, // Adaptação para app android
+  },
+  subTitle: {
+    fontSize: 18,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  image: {
+    width: 292,
+    height: 284,
   },
 });
