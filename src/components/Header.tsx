d5105fa74e62506085721f8da61cf684e39e6df6
renatogual <renatogual@hotmail.com>
2021-04-23 15:19:48 -0300

    Criado funcionalidade para exibir nome digitado pelo usuario na tela principal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import userImg from '../assets/renato.png';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export default function Header() {
+  const [userName, setUserName] = useState<string>();
+
+  useEffect(() => {
+    (async function loadStorageUserName() {
+      const user = await AsyncStorage.getItem('@plantmanager:user');
+      setUserName(user || '');
+    })();
+  }, [userName]);
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.userName}>Renato</Text>
+        <Text style={styles.userName}>{userName}</Text>
       </View>
 
       <Image source={userImg} style={styles.image} />
