3643002226feadc1b4095b4ebce96daa8b335d89
renatogual <renatogual@hotmail.com>
2021-04-23 10:00:03 -0300

    Refatorando

--- src/components/EnviromentButton.tsx
+++ src/components/EnviromentButton.tsx
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: colors.shape,
     borderRadius: 12,
-    marginRight: 5,
+    marginHorizontal: 5,
   },
   containerActive: {
     backgroundColor: colors.green_light,
